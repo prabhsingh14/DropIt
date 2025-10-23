47749ab8fdacfd5a1a38a932d7941997e1ca7a7e
Prabh Singh <prabhsingh@Prabhs-MacBook-Pro.local>
2025-10-23 11:03:15 +0530

    File upload imagekit changes

--- app/api/files/upload/route.ts
+++ app/api/files/upload/route.ts
@@ -5,8 +5,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { and, eq } from "drizzle-orm"
 import ImageKit from "imagekit";
 import { v4 as uuidv4 } from "uuid";
-import { form } from "@heroui/theme";
-import { th } from "zod/v4/locales";
 
 const imagekit = new ImageKit({
     publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
@@ -35,8 +33,9 @@ export async function POST(request: NextRequest) {
                 eq(files.userId, userId),
                 eq(files.isFolder, true)
             )).limit(1)
+
+            if(!parentFolder) return NextResponse.json({ error: "Parent folder not found" }, { status: 400 })
         }
-        if(!parentId) return NextResponse.json({ error: "Parent folder not found" }, { status: 400 })
 
         // Validate MIME type if provided (allow images and PDF)
         if (file.type) {
@@ -108,6 +107,7 @@ export async function POST(request: NextRequest) {
         const [ newFile ] = await db.insert(files).values(fileData).returning();
         return NextResponse.json({ file: newFile })
     } catch (error) {
+        console.error("Error uploading file:", error)
         return NextResponse.json({ error: "Failed to upload file" }, { status: 500 })
     }
 }
